d8a16f5507c192372cbd801ad63b846378b599bb
Sandy Maguire <sandy@sandymaguire.me>
2017-05-02 14:55:39 -0600

    ok

--- src/Control/Monad/Freer/Test.hs
+++ src/Control/Monad/Freer/Test.hs
@@ -18,7 +18,6 @@ module Control.Monad.Freer.Test where
 
 import Control.Monad.Base
 import Control.Monad.Trans.Control
-import Data.Functor.Identity
 import Control.Monad.Freer
 import Control.Monad.Freer.State
 import Data.Type.Equality (type (==))
@@ -26,43 +25,22 @@ import Unsafe.Coerce
 import Control.Exception.Lifted
 
 
-type family Fixed m r where
-  Fixed Identity r = (r :++ '[FixBase Identity r])
-  Fixed m r = (r :++ '[FixBase m r, m])
-
-type family (:++) (xs :: [k]) (ys :: [k]) where
-  '[]       :++ ys = ys
-  (x ': xs) :++ ys = x ': (xs :++ ys)
-
 data FixBase m r a where
-  Fix :: FixBase m r (Eff (Fixed m r) a -> m a)
+  Fix :: FixBase m r (Eff (FixBase m r ': r) a -> m a)
 
-getFix :: forall m r r' a. Member (FixBase m r) r' => Eff r' (Eff (Fixed m r) a -> m a)
+getFix :: forall m r r' a. Member (FixBase m r) r' => Eff r' (Eff (FixBase m r ': r) a -> m a)
 getFix = send $ Fix @m @r
 
 
-runFix :: forall r a
-        . (forall x. Eff (Fixed Identity r) x -> Eff '[FixBase Identity r] x)
-       -> Eff (Fixed Identity r) a
-       -> a
-runFix i = fixed
-  where
-    fixed :: Eff (Fixed Identity r) x -> x
-    fixed = run . runFixNat . i
-
-    runFixNat :: Eff (FixBase Identity r ': r') x -> Eff r' x
-    runFixNat = handleRelay pure (\Fix arr -> arr $ fmap Identity fixed)
-
-
 runFixM :: forall m r a
-         . Monad m
-        => (forall x. Eff (Fixed m r) x -> Eff '[FixBase m r, m] x)
-        -> Eff (Fixed m r) a
+         . BaseMember m r
+        => (forall x. Eff r x -> m x)
+        -> Eff (FixBase m r ': r) a
         -> m a
 runFixM i = fixed
   where
-    fixed :: Eff (Fixed m r) x -> m x
-    fixed = runM . runNat runFixNat . i
+    fixed :: Eff (FixBase m r ': r) x -> m x
+    fixed = i . runNat runFixNat
 
     runFixNat :: FixBase m r x -> m x
     runFixNat Fix = return fixed
@@ -91,20 +69,23 @@ instance (Monad m, LastMember m effs) => BaseMember m effs
 instance (BaseMember m effs, MonadBase b m) => MonadBase b (Eff effs) where
     liftBase = send . (liftBase :: b a -> m a)
 
-
-instance ( r' ~ Fixed m r
-         , BaseMember m r'
+instance ( LastMember m (FixBase m r ': r)
          , MonadBase m m
-         , Member (FixBase m r) r') => MonadBaseControl m (Eff r') where
-  type StM (Eff r') a = a
-  restoreM = return
+         ) => MonadBaseControl m (Eff (FixBase m r ': r)) where
+  type StM (Eff (FixBase m r ': r)) a = a
   liftBaseWith f = do
     i <- getFix @m @r
     send $ f $ unsafeCoerce i
+  restoreM = return
 
-prog :: Eff (Fixed IO '[State Char]) Bool
+prog :: Eff [FixBase IO '[State Char, IO], State Char, IO] Bool
 prog = do
   put 'g'
-  bracket get (\c -> send $ putStrLn $ show c) (\c -> send $ putStrLn $ show c)
+  bracket get (\(c :: Char) -> send $ putStrLn $ show c) (\c -> get >>= send . putStrLn . pure)
+  get >>= send . putStrLn . pure
   return True
 
+main :: IO ()
+main = do
+  runFixM (fmap fst . runM . flip runState 'c') prog
+  return ()
